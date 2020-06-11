f2d21448f29a7f0e5343b423e9227255817c9942
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-06-11 10:18:17 +0600

    Tried to remove a column -removed .yml file

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-var editor = Split(["#left", "#right"], {
+const editor = Split(["#left", "#right"], {
   sizes: [33.3, 66.7],
   minSize: 100,
 });
 // editor.setSizes([50, 50]);
 // editor.collapse(0);
 
-Split(["#top-left", "#mid-left", "#bottom-left"], {
+Split(["#top-left", "#bottom-left"], {
   direction: "vertical",
   sizes: [33, 33, 33],
   minSize: 20,
 });
 
 //icons8 credit
-console.log("Vector from icons8", "https://icons8.com");
 console.log("Vector from icons8", "https://icons8.com");
 console.log("Vector from icons8", "https://icons8.com");
 console.log("Vector from icons8", "https://icons8.com");
